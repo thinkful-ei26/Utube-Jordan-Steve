67a1bedb4d2437a7b4cf966507fd9bd4102ba64b
Jordan <jordanhaddadi@gmail.com>
2018-11-06 14:22:51 -0500

    updating utube

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,177 @@
 const API_KEY = 'AIzaSyA2yOfGt_ejLBH7z3LbtbhyGy7bwi2pONY';
 
+const MOCK_DATA = {
+  "kind": "youtube#searchListResponse",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/B91Ttaa5kd8yB-i8kaHGv4UxtC8\"",
+  "nextPageToken": "CAUQAA",
+  "regionCode": "US",
+  "pageInfo": {
+  "totalResults": 1000000,
+  "resultsPerPage": 5
+  },
+  "items": [
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/dkVsFonSTHSsVgcan59qRr-UCko\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "NwlwJjelgzs"
+  },
+  "snippet": {
+  "publishedAt": "2018-03-30T11:30:00.000Z",
+  "channelId": "UCKy3MG7_If9KlVuvw3rPMfw",
+  "title": "Get ready for LAUGHING SUPER HARD - Best FUNNY DOG videos",
+  "description": "Dogs and puppies are super funny and hilarious, they make us laugh all the time! The hardest TRY NOT TO LAUGH challenge in the World! Just look how all ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/NwlwJjelgzs/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/NwlwJjelgzs/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/NwlwJjelgzs/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "Tiger Productions",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/-HKwERN1Sudb4KGwpxvK76iZ9GE\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "aEzZLXBH3rU"
+  },
+  "snippet": {
+  "publishedAt": "2017-07-08T17:22:26.000Z",
+  "channelId": "UCrBYTP-sa4dtOjMWEBfR_Ng",
+  "title": "*Try Not To Laugh Challenge* Funny Dogs Compilation - Funniest Dog Videos 2017",
+  "description": "\"IMPOSSIBLE CHALLENGE\" Try not to laugh while watching Funny Dog Videos Compilation 2017, The best and funniest dogs videos ever Hope you enjoy ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/aEzZLXBH3rU/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/aEzZLXBH3rU/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/aEzZLXBH3rU/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "All Of Vines",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/ZjiyAF244qW6wir9VN1pxVIfLzk\"",
+  "id": {
+  "kind": "youtube#channel",
+  "channelId": "UCcqLkZqBfBOKmi4vrgkQ6yw"
+  },
+  "snippet": {
+  "publishedAt": "2013-12-21T03:24:11.000Z",
+  "channelId": "UCcqLkZqBfBOKmi4vrgkQ6yw",
+  "title": "Dogs - Topic",
+  "description": "The domestic dog is a member of the genus Canis, which forms part of the wolf-like canids, and is the most widely abundant terrestrial carnivore. The dog and ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://yt3.ggpht.com/-wegtoaGfVQA/AAAAAAAAAAI/AAAAAAAAAAA/B51JbgmtYcQ/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
+  },
+  "medium": {
+  "url": "https://yt3.ggpht.com/-wegtoaGfVQA/AAAAAAAAAAI/AAAAAAAAAAA/B51JbgmtYcQ/s240-c-k-no-mo-rj-c0xffffff/photo.jpg"
+  },
+  "high": {
+  "url": "https://yt3.ggpht.com/-wegtoaGfVQA/AAAAAAAAAAI/AAAAAAAAAAA/B51JbgmtYcQ/s800-c-k-no-mo-rj-c0xffffff/photo.jpg"
+  }
+  },
+  "channelTitle": "Dogs - Topic",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/viybePy1jQvnTdMKEjnZy7y8pAw\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "rZVpo5HbDf8"
+  },
+  "snippet": {
+  "publishedAt": "2018-09-20T17:04:55.000Z",
+  "channelId": "UCz35G-z-ZB5rTma2yka7G8A",
+  "title": "Ep 8: WIENER DOG PRISON BREAK - Funny Dogs Escaping Jail!",
+  "description": "In this video, Crusoe and Oakley the miniature #dachshunds get locked up in jail for a misdewiener crime and then have to find a way to bust themselves out in ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/rZVpo5HbDf8/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/rZVpo5HbDf8/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/rZVpo5HbDf8/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "Crusoe the Celebrity Dachshund",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/ewKxgDGd2zeva-5noZeowJrGv80\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "-G16EX6b8Xg"
+  },
+  "snippet": {
+  "publishedAt": "2018-07-24T17:00:01.000Z",
+  "channelId": "UC4rlAVgAK0SGk-yTfe48Qpw",
+  "title": "12 Harmful Things You Do to Your Dog Without Realizing It",
+  "description": "How to Keep Your Dog Healthy. If you're a dog owner, you surely want your pet to be healthy and to live a long and happy life. But you might sometimes do ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/-G16EX6b8Xg/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/-G16EX6b8Xg/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/-G16EX6b8Xg/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "BRIGHT SIDE",
+  "liveBroadcastContent": "none"
+  }
+  }
+  ]
+  }
+
 /*
   We want our store to hold an array of "decorated" video objects - i.e. objects that
   have been transformed into ONLY the necessary data we're displaying on our page. 
@@ -74,19 +246,15 @@ fetchVideos('dogs', function(response) {
  * @param   {object} response - should match Youtube API response shape
  * @returns {array}
  */
+
 function decorateResponse(response) {
-  arrayOfObjects = [];
-  videoObject = { id: '', title: '', thumbnail: '' };
-  fetchVideos('Thinkful', function(response) {
-    response.items.map(item => {
-      videoObject.title = item.snippet.title;
-      videoObject.thumbnail = item.snippet.thumbnails.high.url;
-      videoObject.videoId = item.snippet.videoId;
-      arrayOfObjects.push(videoObject);
+    return response.items.map(item => {
+      return {
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.high.url,
+      id: item.snippet.videoId,
+      }
     });
-  });
-  console.log(arrayOfObjects);
-  return arrayOfObjects;
 }
 // TASK:
 // 1. Map through the response object's `items` array
@@ -107,7 +275,17 @@ function decorateResponse(response) {
 // TASK:
 // 1. Using the decorated object, return an HTML string containing all the expected
 // TEST IT!
-const generateVideoItemHtml = function(video) {};
+const generateVideoItemHtml = function(video) {
+  return ` 
+    <li data-video-id="${video.id}">
+      <imc src="${video.thumbnail}" />
+        <h3>${video.title}</h3>
+    </li>
+  `;
+};
+
+const decoratedObjects = decorateResponse(MOCK_DATA);
+console.log(generateVideoItemHtml(decoratedObjects[0]));
 
 /**
  * @function addVideosToStore
@@ -117,7 +295,14 @@ const generateVideoItemHtml = function(video) {};
 // TASK:
 // 1. Set the received array as the value held in store.videos
 // TEST IT!
-const addVideosToStore = function(videos) {};
+const addVideosToStore = function(videos) {
+  store.videos = videos;
+};
+
+const videos = decorateResponse(MOCK_DATA);
+addVideosToStore(videos);
+
+
 
 /**
  * @function render
@@ -127,7 +312,16 @@ const addVideosToStore = function(videos) {};
 // 1. Map through `store.videos`, sending each `video` through `generateVideoItemHtml`
 // 2. Add this array of DOM elements to the appropriate DOM element
 // TEST IT!
-const render = function() {};
+const render = function() {
+    const html = store.videos.map(video => generateVideoItemHtml(video));
+    $('.results').html(html);
+};
+
+fetchVideos('dogs', (response) => {
+  const decorated = decorateResponse(response);
+  addVideosToStore(decorated);
+  render();
+});
 
 /**
  * @function handleFormSubmit
@@ -145,7 +339,15 @@ const render = function() {};
 //      `addVideosToStore` function
 //   g) Inside the callback, run the `render` function
 // TEST IT!
-const handleFormSubmit = function() {};
+const handleFormSubmit = function() {
+ $('.')on.('submit', event => {
+   event.preventDefault();
+
+
+   render();
+ })
+
+};
 
 // When DOM is ready:
 $(function() {
